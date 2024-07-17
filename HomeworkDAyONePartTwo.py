7eec30f3185e9971fdd06e71bb80af1951b8e8b5
jedaniels3933 <jedaniels3933@gmail.com>
2024-07-17 18:30:39 -0400

    First commit

---
+++ HomeworkDAyONePartTwo.py
@@ -0,0 +1,38 @@
+#Task 1: Code Correction Given below are some variable assignments. Your task is to identify the data type of each variable using type() and then use the print() function to print it out:
+
+variable_a = "Hello, World!"
+variable_b = 23
+variable_c = 3.14
+variable_d = True
+
+print(type(variable_a))
+
+print(type(variable_b)) 
+
+print(type(variable_c))
+
+print(type(variable_d))  
+
+#Task 1: Grocery Store Math Calculate the total cost of three items you'd commonly find in a grocery store, given their individual prices. With the variables given below find the total cost of this grocery list.
+
+bread = 2.75
+peanut_butter= 4.50
+jelly = 3.23
+total_cost = bread + peanut_butter + jelly
+
+print(total_cost)
+
+
+ 
+      
+
+
+
+
+      
+
+
+
+
+
+
---
+++ HomeworkDayOneFinished.py
@@ -0,0 +1,43 @@
+#Task 1: Code Correction Below is a piece of Python code with incorrect indentation. Your task is to correct it.
+
+weather = "sunny"
+
+if weather == "sunny":
+    print("Wear sunglasses!")
+else:
+    print("Take an umbrella!")
+
+#Task 2: Your Mood Today  Create another if statement evaluating the users mood. Ask the user how they feel today using input(). If they say "happy", print "That's great to hear!", and if they say "sad", print "I hope your day gets better!". Ensure your if statement is properly indented.
+ask = input("How are you feeling today?")
+if ask == "happy" : 
+    print("That's great to hear!")
+elif ask == "sad":
+    print("I hope your day gets better!")
+
+   
+#Task 1: Code Correction Given below are some variable assignments. Your task is to identify the data type of each variable using type() and then use the print() function to print it out:
+
+variable_a = "Hello, World!"
+variable_b = 23
+variable_c = 3.14
+variable_d = True
+
+print(type(variable_a))
+
+print(type(variable_b)) 
+
+print(type(variable_c))
+
+print(type(variable_d))  
+
+#Task 1: Grocery Store Math Calculate the total cost of three items you'd commonly find in a grocery store, given their individual prices. With the variables given below find the total cost of this grocery list.
+
+bread = 2.75
+peanut_butter= 4.50
+jelly = 3.23
+total_cost = bread + peanut_butter + jelly
+
+print(total_cost)
+
+
+
